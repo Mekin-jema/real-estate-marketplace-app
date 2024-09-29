a9905475ef5bb4bba6f64916e37409dc3145a4d0
Mekin Jemal <mekinjemal999@gmail.com>
2024-09-29 23:16:04 +0300

    create signin api route funcationality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -20,6 +21,38 @@ export const signup = async (req, res, next) => {
       message: "User Created Successfully",
       user: rest,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const singin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credential"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .json({
+        currentUser: rest,
+        message: "User Logged In Successfully",
+      });
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
-import { signup } from "../controllers/auth.controller.js";
+import { signup, singin } from "../controllers/auth.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", singin);
 
 export default router;
