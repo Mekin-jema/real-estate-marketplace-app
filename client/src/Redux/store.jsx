807d528916e63d1a7d12c8a25157d87a672cee8b
Mekin Jemal <mekinjemal999@gmail.com>
2024-09-30 14:37:33 +0300

    add Redux-persist

--- client/src/Redux/store.jsx
+++ client/src/Redux/store.jsx
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+const persistConfig = {
+  key: "root ",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (
     getDefaultMiddleware // eslint-disable-line no-unused-vars
   ) =>
@@ -14,3 +25,4 @@ const store = configureStore({
 });
 
 export default store;
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./Redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
