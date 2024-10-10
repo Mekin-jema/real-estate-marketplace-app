fd932bac1cf0f50c93bb3021ec2e517d2e0387a4
Mekin Jemal <mekinjemal999@gmail.com>
2024-10-10 23:46:38 +0300

    fix once again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
+import UpdateListing from "./pages/UpdateListing";
 const App = () => {
   return (
     <BrowserRouter>
